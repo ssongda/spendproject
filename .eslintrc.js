d0dcc34c30eb3558713970a8956a443f91f7dccb
HIRO-DESKTOP\Hiro <tonakatomato@gmail.com>
2024-09-20 22:29:51 +0900

    - add eslintrc and prettierrc files - delete tailwaindcss

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: '@babel/eslint-parser',
+  rules: {
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+  plugins: ['unused-imports'],
+};
